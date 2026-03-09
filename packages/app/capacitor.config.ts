df07e14838d8e9a4d6a175e16a7bc157a5cb75e1
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-09 22:51:28 +0700

    update cap

--- packages/app/capacitor.config.ts
+++ packages/app/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
 	appId: 'io.github.hieudoanm.poker',
-	appName: '@poker/app',
+	appName: 'Poker',
 	webDir: '../../docs',
 	android: { path: 'mobile/android' },
 	ios: { path: 'mobile/ios' },
